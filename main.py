39f8f3b9c27fa9bfa514f87c7f67eea05902fde5
GaSkia <gabry.schiavo00@gmail.com>
2025-08-05 21:39:54 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import platform
+import sys
+from modules.print import unix
+
+
+print("Ciao, inserire il percorso completo del file. " +
+      "Non deve contenere spazi: ")
+
+filepath = input()
+
+print("Indicare il carattere usato come delimitatore: ")
+delimiter = input()
+
+if len(delimiter) != 1:
+    sys.exit("Il delimitatore deve essere composto da un carattere solo.")
+
+
+if platform.system() == "Windows":
+    print("Non ancora implementato.")
+
+elif platform.system() in ("Linux", "Darwin"):
+    print("INFO: executing on unix platform")
+    unix(filepath=filepath, delimiter=delimiter)
+
+
+else:
+    sys.exit("Piattaforma non supportata.")
