8e2e897163bd320d7f9a7f2f18c5b9038f35a765
GaSkia <gabry.schiavo00@gmail.com>
2025-08-05 21:43:45 +0200

    (feature) add linux modules for printing

---
+++ modules/print.py
@@ -0,0 +1,50 @@
+import csv
+import tempfile
+
+# def windows():
+#     from win32printing import Printer
+#
+#     with open(filepath, newline='\r\n') as f:
+#         reader = csv.reader(f, 'r', delimiter=delimiter)
+#
+labels = []
+
+
+def unix(filepath: str, delimiter: str):
+    import cups
+
+    with open(filepath, newline='') as f:
+        reader = csv.reader(f, delimiter=delimiter)
+        for row in reader:
+            labels.append(row)
+
+    conn = cups.Connection()
+    printers = conn.getPrinters()
+    printer_names = list(printers.keys())
+    number = 0
+    for printer in printers:
+        print(f'[{number}]', printer, printers[printer]["device-uri"])
+        number = number + 1
+    print('Seleziona stampante: ')
+    choice = int(input())
+    printer = printers[printer_names[choice]]["printer-info"]
+    for label in labels:
+        with tempfile.NamedTemporaryFile(delete=False,
+                                         suffix=".txt", mode='w') as tmp:
+            text = f"""
+                    ^XA
+                    ^FO50,50^A0N,30,30^FD{label[0]}^FS
+                    ^FO50,100^BY2
+                    ^BCN,100,Y,N,N
+                    ^FD^FS
+                    ^XZ
+                   """
+            print('testo: ', text)
+            print('stampante: ', printer)
+            tmp.write(text)
+            tmp_path = tmp.name
+            print('file', tmp_path)
+            tmp.flush()
+
+        job_id = conn.printFile(printer, tmp_path, label[0], {})
+        print(f"Stampando {label}. Job ID: {job_id}")
